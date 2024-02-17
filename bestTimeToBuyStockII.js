28d55775052eb1f067942a405b1c4cac7f1354ee
Nitesh <nitesh0581@gmail.com>
2024-02-17 23:46:54 +0530

    2nd ques

---
+++ bestTimeToBuyStockII.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+var maxProfit = function(prices) {
+    let sum=0;
+    for(let i=0; i<prices.length-1; i++){
+        if(prices[i]<prices[i+1]){
+            sum+=prices[i+1]-prices[i];
+        }
+    }
+    return sum;
+};
