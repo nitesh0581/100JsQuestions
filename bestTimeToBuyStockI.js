300f193934ca75ce259ae779a652a08d8bd015f9
nitesh <nitesh0581@gmail.com>
2024-02-20 01:06:28 +0530

    done

--- bestTimeToBuyStockI.js
+++ bestTimeToBuyStockI.js
@@ -4,9 +4,9 @@ var maxProfit = function(prices) {
     for(i of prices){
         minNum=Math.min(minNum,prices[i]);
         maxProfit=Math.max(prices[i]-minNum,maxProfit);
-    }
+    } 
     return maxProfit;
 };
 //prices=[7,1,5,3,6,4]
 //min-7 =>7,1,1,1,1,1
-//max-0 =>0,0,4,4,5,5
+//max-0 =>0,0,4,4,5,5 
