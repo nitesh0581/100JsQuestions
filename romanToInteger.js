c15febf76dc658f1d1e68baa76befb82e7a81e1f
nitesh <nitesh0581@gmail.com>
2024-02-21 01:38:04 +0530

    romanToInteger with optimise approach

--- romanToInteger.js
+++ romanToInteger.js
@@ -1,49 +1,20 @@
-/**
- * @param {string} s
- * @return {number}
- */
- //XXXX
- var romanToInt = function(s) {
+var romanToInt = function(s) {
+    const romanValues={
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000
+    }
     let result=0;
-    for(let i=0; i<s.length; i++){
-        if(s.charAt(i)==='I'){
-            if(s.charAt(i+1)==='V'){
-                result+=4;
-                i=i+1;
-            }else if(s.charAt(i+1)==='X'){
-                result+=9;
-                i=i+1;
-            }else{
-                result+=1;
-            }
-        }else if(s.charAt(i)==='X'){
-            if(s.charAt(i+1)==='L'){
-                result+=40;
-                i=i+1;
-            }else if(s.charAt(i+1)==='C'){
-                result+=90;
-                i=i+1;
-            }else{
-                result+=10;
-            }           
-        }else if(s.charAt(i)==='C'){
-            if(s.charAt(i+1)==='D'){
-                result+=400;
-                i=i+1;
-            }else if(s.charAt(i+1)==='M'){
-                result+=900;
-                i=i+1;
-            }else{
-                result+=100;
-            }
-        }else if(s.charAt(i)==='L'){
-            result+=50;
-        }else if(s.charAt(i)==='D'){
-            result+=500;
-        }else if(s.charAt(i)==='M'){
-            result+=1000;
-        }else if(s.charAt(i)==='V'){
-            result+=5;
+    for(let i=s.length-1; i>=0; i--){
+        let currVal=romanValues[s[i]];
+        if(i<s.length-1 && currVal<romanValues[s[i+1]]){
+            result-=currVal;
+        }else{
+            result+=currVal;
         }
     }
     return result;
