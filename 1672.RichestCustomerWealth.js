5f201eeb9013f0785f3cac90f02ef7e88730ad9b
nitesh <nitesh0581@gmail.com>
2024-03-03 22:30:03 +0530

    1672. Richest Customer Wealth

---
+++ 1672.RichestCustomerWealth.js
@@ -0,0 +1,14 @@
+var maximumWealth = function(accounts) {
+    let max=0;
+    for(let i=0;i<accounts.length; i++){
+            let sum=0;
+        for(let j=0;j<accounts[0].length;j++){
+            sum+=accounts[i][j];
+        }
+        if(sum>max){
+            max=sum;
+        }
+    }
+    return max;
+};
+//q: https://leetcode.com/problems/richest-customer-wealth/
