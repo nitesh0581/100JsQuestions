a247990d0e9244618827fd9df2267c4cc5d3ed31
nitesh <nitesh0581@gmail.com>
2024-03-08 00:13:27 +0530

    sumIndicesWithKSetBits

---
+++ 2859.SumofValuesatIndicesWithKSetBits.js
@@ -0,0 +1,16 @@
+var sumIndicesWithKSetBits = function(nums, k) {
+    let sum=0;
+    for(let i=0; i<nums.length; i++){
+        let base2=i.toString(2);
+        let count=0;
+        for(let j=0; j<base2.length; j++){
+            if(base2[j]==1){
+                count++;
+            }
+        }
+        if(count==k){
+            sum+=nums[i];
+        }
+    }
+    return sum;
+};
