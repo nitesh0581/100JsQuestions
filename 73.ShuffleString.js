6c5e4b5099068d0470c5d88ea19d37a7407b992d
nitesh <nitesh0581@gmail.com>
2024-03-18 01:00:19 +0530

    restoreString

--- 73.ShuffleString.js
+++
@@ -1,9 +0,0 @@
-var restoreString = function(s, indices) {
-    const result=[];
-    for(let i=0; i<indices.length; i++){
-        result[indices[i]]=s[i];
-    }
-    return result.join("");
-};
-
-//Q: https://leetcode.com/problems/shuffle-string/
