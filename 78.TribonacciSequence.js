90fdc73c52ef1953b67a9df89eea689458b12a08
nitesh <nitesh0581@gmail.com>
2024-03-24 00:23:05 +0530

    Tribonacci sequence

--- 78.TribonacciSequence.js
+++ 78.TribonacciSequence.js
@@ -1,1 +1,13 @@
-b
+const mem={
+    0:0,
+    1:1,
+    2:1
+}
+var tribonacci = function(n) {
+if(mem[n]!== undefined){
+    return mem[n];
+}
+return mem[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+};
+
+//Q:https://leetcode.com/problems/n-th-tribonacci-number/
