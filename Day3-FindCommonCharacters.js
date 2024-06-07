60588d9da082e6e33f6c08780a0f9a4c53996273
nitesh <nitesh0581@gmail.com>
2024-06-07 11:57:11 +0530

    day3-findcommoncharacter

---
+++ Day3-FindCommonCharacters.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let newStr=s.trim();
+    let count=0;
+    for(let i=newStr.length-1; i>=0; i--){
+        if(newStr[i]==" "){
+            return count;
+        }else{
+            count++;
+        }
+    }
+     return count;
+};
